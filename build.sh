e5af6edc344e3ae7934e11b13ddb64db91f7105c
kosta709 <kosta@codefresh.io>
2017-06-20 13:50:06 +0300

    imporved build script

--- build.sh
+++ build.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
 
+save_images() {
+    local IMAGES_LIST=$1
+    local IMAGES_SAVE=""
+    while read image; do
+      docker pull ${image}
+      IMAGES_SAVE="${IMAGES_SAVE} $image"
+    done < $IMAGES_LIST
+    echo docker save -o ${IMAGES_DIR}/${IMAGES_LIST}.tar ${IMAGES_SAVE}
+    docker save -o ${IMAGES_DIR}/${IMAGES_LIST}.tar ${IMAGES_SAVE}
+}
+
 DIR=$(dirname $0)
 
 echo "Staring $0 at $(date) "
 echo "DOCKER_HOST=$DOCKER_HOST"
 
-IMAGES_INIT=cf-images-init
-IMAGES_RUN=cf-images-run
+IMAGES_LIST_INIT=cf-images-init
+IMAGES_LIST_RUN=cf-images-run
 
 IMAGES_DIR=${DIR}/cfimages
 mkdir -p $IMAGES_DIR
 
-IMAGES_INIT_SAVE=""
-while read image; do
-  docker pull ${image}
-  IMAGES_INIT_SAVE="${IMAGES_INIT_SAVE} $image"
-done < $IMAGES_INIT
-echo docker save -o ${IMAGES_DIR}/${IMAGES_INIT}.tar ${IMAGES_INIT_SAVE}
-docker save -o ${IMAGES_DIR}/${IMAGES_INIT}.tar ${IMAGES_INIT_SAVE}
+save_images ${IMAGES_LIST_INIT}
 
+save_images ${IMAGES_LIST_RUN}
 
-IMAGES_RUN_SAVE=""
-while read image; do
-  docker pull ${image}
-  IMAGES_RUN_SAVE="${IMAGES_RUN_SAVE} $image"
-done < $IMAGES_RUN
-echo docker save -o ${IMAGES_DIR}/${IMAGES_RUN}.tar ${IMAGES_RUN_SAVE}
-docker save -o ${IMAGES_DIR}/${IMAGES_RUN}.tar ${IMAGES_RUN_SAVE}
--- dockerfile.sh
+++ dockerfile.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-
-
+IMAGE_NAME=codefresh/cf-dind:17.05.0-overlay-v2
 BUILD_DOCKER_ADDR="192.168.99.100"
 BUILD_DOCKER_PORT=9998
 BUILD_DOCKER_HOST=tcp://${BUILD_DOCKER_ADDR}:${BUILD_DOCKER_PORT}
 
 docker run --name cf-dind-build-daemon -p ${BUILD_DOCKER_PORT}:${BUILD_DOCKER_PORT} -d --privileged docker:17.05.0-ce-dind dockerd -H tcp://0.0.0.0:${BUILD_DOCKER_PORT}
 
-docker build --build-arg BUILD_DOCKER_HOST=${BUILD_DOCKER_HOST} -t codefresh/cf-dind:17.05.0-overlay-v2 .
+docker build --build-arg BUILD_DOCKER_HOST=${BUILD_DOCKER_HOST} -t ${IMAGE_NAME} .
