3045a6eb44b4e2ff1105c7d55f1c6e7971303cd1
kosta709 <kosta@codefresh.io>
2017-08-03 09:34:13 +0300

    adding cleaner

--- docker-cleaner.sh
+++ docker-cleaner.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "$(date)" >> /var/log/cleaner.log
-#docker run --rm --name rt-cleaner -v /var/run/docker.sock:/var/run/docker.sock:rw --label io.codefresh.owner=codefresh -e GRACE_PERIOD_SECONDS=86400 --cpu-shares=10 codefresh/cf-runtime-cleaner:latest ./docker-gc >> /var/log/cleaner.log 2>&1
+echo "$0 - $(date)" >> /var/log/cleaner.log
+docker run --rm --name rt-cleaner -v /var/run/docker.sock:/var/run/docker.sock:rw --label io.codefresh.owner=codefresh -e GRACE_PERIOD_SECONDS=86400 --cpu-shares=10 codefresh/cf-runtime-cleaner:latest ./docker-gc >> /var/log/cleaner.log 2>&1
