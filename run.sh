a48400e70945e8c821875731136fc2ae9149d2dd
kosta709 <kosta@codefresh.io>
2017-09-27 17:26:51 +0300

    cf-dind:v6 - added client-cert path

--- run.sh
+++ run.sh
@@ -52,6 +52,13 @@ fi
 rm -fv /var/run/docker.pid
 mkdir -p /var/run/codefresh
 
+# Setup Client certificate ca
+if [[ -n "${CODEFRESH_CLIENT_CA_DATA}" ]]; then
+  CODEFRESH_CLIENT_CA_FILE=${CODEFRESH_CLIENT_CA_FILE:-/etc/ssl/cf-client/ca.pem}
+  mkdir -pv $(dirname ${CODEFRESH_CLIENT_CA_FILE} )
+  echo ${CODEFRESH_CLIENT_CA_DATA} | base64 -d >> ${CODEFRESH_CLIENT_CA_FILE}
+fi
+
 echo "$(date) - Starting dockerd with /etc/docker/daemon.json: "
 cat /etc/docker/daemon.json
 
